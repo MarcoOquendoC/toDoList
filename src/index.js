6e911791a0c50eec18884d9ef6045ed0b4116014
Marco Oquendo <marcooquendoc@gmail.com>
2022-08-12 23:55:13 -0500

    add render feature as data methods

--- src/index.js
+++ src/index.js
@@ -1,21 +1,63 @@
-import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
+/* import _ from 'lodash'; */
+/* import 'style.css'; */
 
-function component() {
-  const element = document.createElement('div');
+const list = document.querySelector('#list');
+const form = document.querySelector('#form');
+const description = document.querySelector('#description');
+let id = 0;
+let data = [
+  {
+    _description: 'Wash the dishes',
+    _completed: false,
+    _id: 0,
+  },
+  {
+    _description: 'Complete To Do list project',
+    _completed: false,
+    _id: 0,
+  },
+  {
+    _description: 'Great it works',
+    _completed: false,
+    _id: 0,
+  }
+];
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  // Add the image to our existing div.
-  const myIcon = new Image();
-  myIcon.src = Icon;
-
-  element.appendChild(myIcon);
-
-  return element;
+class Task {
+  constructor(description, id = 0) {
+    this._description = description;
+    this._completed = false;
+    this._id = id;
+  }
 }
 
-document.body.appendChild(component());
+
+class Data {
+  static setIndexes() {
+    data.forEach((element, index) => {
+      element._id = index + 1;
+    });
+  }
+
+  static render() {
+    list.innerHTML = '';
+    data.forEach((element, index) => {
+      const checked = element._completed? 'checked': '';
+      list.innerHTML += 
+      `
+        <li class="listItem" draggable="true">
+          <input id="check${index}" type="checkbox" name="completed" class="check" ${checked}>
+          <label for="check${index}">${element._id}</label>
+          <input value="${element._description}" type="text" name="description" class="description active" disabled>
+          <div>
+            <button type="button" class="iconBtn editBtn"><strong>🖊</strong></button>
+            <button type="button" class="iconBtn delBtn"><strong>🗑</strong></button>
+          </div>
+        </li>
+      `
+    });
+  }
+}
+
+Data.setIndexes()
+Data.render()
