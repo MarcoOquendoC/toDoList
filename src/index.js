d740d46c059af33a97889807db13ce00dd374515
Marco Oquendo <marcooquendoc@gmail.com>
2022-08-13 00:41:18 -0500

    Linter error corrections

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
 /* import _ from 'lodash'; */
-/* import 'style.css'; */
+import './style.css';
 
 const list = document.querySelector('#list');
 
 const data = [
   {
+    id: 0,
+    editable: false,
+    completed: true,
     description: 'Wash the dishes',
-    completed: false,
-    id: 0,
   },
   {
+    id: 0,
+    editable: true,
+    completed: false,
+    description: 'currently editable',
+  },
+  {
+    id: 0,
+    editable: false,
+    completed: false,
     description: 'Complete To Do list project',
-    completed: false,
-    id: 0,
-  },
-  {
-    description: 'Great it works',
-    completed: false,
-    id: 0,
   },
 ];
 
@@ -32,14 +35,16 @@ class Data {
     list.innerHTML = '';
     data.forEach((element, index) => {
       const checked = element.completed ? 'checked' : '';
+      const active = element.editable ? 'active' : '';
+      const disable = element.editable ? '' : 'disabled';
       list.innerHTML
       += `
-        <li class="listItem" draggable="true">
+        <li class="listItem" draggable="${!element.editable}">
           <input id="check${index}" type="checkbox" name="completed" class="check" ${checked}>
           <label for="check${index}">${element.id}</label>
-          <input value="${element.description}" type="text" name="description" class="description active" disabled>
+          <input value="${element.description}" type="text" name="description" class="description" ${disable}>
           <div>
-            <button type="button" class="iconBtn editBtn"><strong>🖊</strong></button>
+            <button type="button" class="iconBtn editBtn ${active}"><strong>🖊</strong></button>
             <button type="button" class="iconBtn delBtn"><strong>🗑</strong></button>
           </div>
         </li>
