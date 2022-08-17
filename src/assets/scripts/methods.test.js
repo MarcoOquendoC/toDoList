c10e0369bddea05f4bde13b45adaff07338b681f
Marco Oquendo <marcooquendoc@gmail.com>
2022-08-17 15:07:29 -0500

    test add feature

--- src/assets/scripts/methods.test.js
+++ src/assets/scripts/methods.test.js
@@ -2,23 +2,22 @@
 
 import Method from './methods.js'
 
-describe ('Add task feature', () => {
-  window.localStorage = Storage.prototype;
-  document.body.innerHTML = '<ol id="list"></ol>';
+window.localStorage = Storage.prototype;
+document.body.innerHTML = '<ol id="list"></ol>';
 
+describe ('Add task feature', () => {
   test('correct adding feature', () => {
-    let tasks = Method.add('Task 1')
+    let tasks = Method.add('Task 1');
     expect(tasks).toHaveLength(1);
-    tasks = Method.add('Task 2')
+    tasks = Method.add('Task 2');
     expect(tasks).toHaveLength(2);
   });
   test('Empty task description', () => {
-    let tasks = Method.add('')
+    let tasks = Method.add('');
     expect(tasks).toBe(-1);
   });
   test('HTML with correct number of <li> elements', () => {
-    let htmlTasks = document.querySelectorAll('.listItem')
+    let htmlTasks = document.querySelectorAll('.listItem');
     expect(htmlTasks).toHaveLength(2);
   });
-
-})
+});
